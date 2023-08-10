fea6fa38366813b75ae3775a8d049bda323f8161
Elkin08 <elkinandreslm@ufps.edu.co>
2023-08-09 19:29:15 -0500

    Clase persona modificada

---
+++ src/com/aprendiendogith/controladores/MainViewController.java
@@ -0,0 +1,50 @@
+package com.aprendiendogith.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class MainViewController {
+
+    @FXML
+    private ListView<String> lstLista;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    private TextField txtEdad;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    void actualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void listar(ActionEvent event) {
+
+    }
+
+}
--- src/com/aprendiendogith/modelos/Persona.java
+++ src/com/aprendiendogith/modelos/Persona.java
@@ -1,5 +1,72 @@
 package com.aprendiendogith.modelos;
 
 public class Persona {
+	
+	private int id;
+	
+	private String cedula;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private short edad;
+	
+	public Persona() {
+		
+	}
+
+	public Persona(String cedula, String nombre, String apellido, short edad) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public short getEdad() {
+		return edad;
+	}
+
+	public void setEdad(short edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "id= " + id + ", nombre= " + nombre + ", apellido= " + apellido + ", edad= " + edad + "";
+	}
+
 
 }
